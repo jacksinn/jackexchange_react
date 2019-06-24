fb1b3e893f9150cb0e5ed8d3caaf4f555200c636
Steven <scjackson@linux.com>
2019-06-24 17:00:16 -0400

    Move components, cleanup from lesson, install material ui and axios

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Lessons from './components/Lessons';
-import LessonsFunc from './components/LessonsFunc';
+import LessonsList from './components/Lessons/LessonsList';
 
 function App() {
-  // let lessons = [
-  //   'Lesson 1',
-  //   'Lesson 2',
-  // ]
-
-  // let message = 'Hello from a var'
-
-  // TODO: Challenge: Create a List of Courses
-  //  Can I see a list of courses?
-  //  Bonus: Can I see how many videos are in a course?
-  //  Bonus: Make the list of courses use a course component and 
-  //    pass the component values with props.
-
-  // TODO: Challenge: Create a Course View
-  //  Can I view the course?
-
-  // TODO: Install extra dependencies
-  //Bring in Material Theme
-  // npm install @material-ui/core
-  // npm install @material-ui/icons
-
-  // Bring in Axios
-  // npm install axios
-
   return (
     <div className="App">
-      <LessonsFunc name="Fabio" youtube="hi"></LessonsFunc>
-      <LessonsFunc>
+      <LessonsList>
         I really really want to put something here.
-      </LessonsFunc>
+      </LessonsList>
     </div>
   );
 }
