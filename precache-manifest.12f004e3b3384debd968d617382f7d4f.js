4f83256e845a9abccb261c5f8151a1e4a8beaa65
Steven <scjackson@linux.com>
2019-07-09 08:39:31 -0400

    Updates

---
+++ precache-manifest.12f004e3b3384debd968d617382f7d4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08d1d55dafc2ddb5c7495694c87232f",
+    "url": "/jackexchange_react/index.html"
+  },
+  {
+    "revision": "7d217ec66ff392c4be1f",
+    "url": "/jackexchange_react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2cd7659876bdc0826b7d",
+    "url": "/jackexchange_react/static/js/2.49407ccd.chunk.js"
+  },
+  {
+    "revision": "7d217ec66ff392c4be1f",
+    "url": "/jackexchange_react/static/js/main.ba52b5bb.chunk.js"
+  },
+  {
+    "revision": "e0979646f7ba44a1d080",
+    "url": "/jackexchange_react/static/js/runtime~main.adbd1594.js"
+  }
+]);
